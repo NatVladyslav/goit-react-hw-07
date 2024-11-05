9bf9c9946ad247b27167dc3f5cb81ce2d5808c77
Vladyslav Natalych <v.natalich05@gmail.com>
2024-11-05 19:39:24 +0200

    Delete persisGate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store.js';
+
+import { store } from './redux/store.js';
 import 'modern-normalize';
 import './reset.css';
 import './index.css';
@@ -11,9 +11,7 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice.js';
 import { filterReducer } from './filtersSlice.js';
-import storage from 'redux-persist/lib/storage';
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
-const contactsConfig = {
-  key: 'contacts', // ключ з localStorage
-  storage,
-  // whitelist: ['items'], // -  виключення синхронізації поля
-};
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-export const persistor = persistStore(store);
